feafebd18fc0b4a3a2db6e804448e91d8d21a22e
Akash Fokane <63790654+akash-fokane@users.noreply.github.com>
2026-03-31 23:20:30 +0530

    Initial commit

---
+++ include/main.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define IMAGE_WIDTH 256
+#define IMAGE_HEIGHT 256
---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "../include/main.h"
+
+int main()
+{
+    uint16_t image_width = IMAGE_WIDTH, image_height = IMAGE_HEIGHT;
+
+    return 0;
+}
